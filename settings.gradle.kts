2d18a78b8048d3855f4f41ac9314310cd7b5268a
Christina Black <xtina@mabl.com>
2026-02-25 11:35:00 -0500

    tweaks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "intellij-theme-randomizer"
+rootProject.name = "intellij-theme-randomizer-fork"
